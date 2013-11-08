aa99d3b2b65ba3da6bd96efea1bd9769c074fa31
jeremyBass <jeremybass@cableone.net>
2013-11-08 15:41:00 -0800

    added logging of the API
    also added the first set of options

---
+++ app/code/community/Wsu/Logger/Model/Observer.php
@@ -0,0 +1,30 @@
+<?php
+class Wsu_Logger_Model_Observer {
+	public function controller_action_postdispatch_api(Varien_Event_Observer $observer) {
+
+		$enable = Mage::getStoreConfig('wsu_logger/apilog/enablelogging');
+		if (!$enable) {
+			return;
+		}
+
+		$controllerAction = $observer->getControllerAction(); /* @var $controllerAction Mage_Api_IndexController */
+
+		$logFormat = Mage::getStoreConfig('wsu_logger/apilog/logformat');
+		if (empty($logFormat)) {
+			$logFormat = 'wsu_logger: No logformat configuration found in wsu_logger/apilog/logformat';
+		}
+
+		$replace = array(
+			'###REQUESTURI###' => $controllerAction->getRequest()->getRequestUri(),
+			'###CLIENTIP###' => $controllerAction->getRequest()->getClientIp(),
+			'###REQUEST###' => file_get_contents('php://input'),
+			'###RESPONSE###' => $controllerAction->getResponse()->getBody()
+		);
+
+		$message = str_replace(array_keys($replace), array_values($replace), $logFormat);
+		$fileName = Mage::getStoreConfig('wsu_logger/apilog/logfilename');
+
+		Mage::log($message, null, $fileName);
+	}
+
+}
---
+++ app/code/community/Wsu/Logger/controllers/LoggerController.php
@@ -0,0 +1,4 @@
+<?php
+class Wsu_Logger_Adminhtml_Wsu_LoggerController extends Mage_Adminhtml_Controller_Action {
+
+}
